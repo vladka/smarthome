668d53718e8fee952002ceabf0a29ac1857bb843
Vladka <xvladka@gmail.com>
2021-04-04 19:59:08 +0200

    cleanup

--- pages/smart.tsx
+++ pages/smart.tsx
@@ -3,8 +3,38 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import DeviceList from '../components/deviceList'
 
+// const socket = () => {
+//   const ws = new WebSocket('ws://localhost:3000/api/graphqlSubscriptions')
+
+//   ws.onopen = function () {
+//     // Web Socket is connected, send data using send()
+//     //  ws.send("Message to send");
+//     //  alert("Message is sent...");
+//     console.log('OPENED')
+//   }
+
+//   ws.onmessage = function (evt) {
+//     console.log('EVVT', evt)
+//   }
+
+//   ws.onclose = function () {
+//     // websocket is closed.
+//     console.log('Connection is closed...')
+//   }
+//   return ws
+// }
 
 const Index = () => {
+  // const onChangeName = () => {
+  //   updateNameMutation({
+  //     variables: {
+  //       name: newName,
+  //     },
+  //   })
+  // }
+  useEffect(() => {
+    // window.ws001 = socket()
+  }, [])
 
   return (
     <div className={styles.container}>
@@ -29,5 +59,19 @@ const Index = () => {
     </div>
   )
 }
+
+// export async function getStaticProps() {
+//   const apolloClient = initializeApollo()
+
+//   await apolloClient.query({
+//     query: ViewerDocument,
+//   })
+
+//   return {
+//     props: {
+//       initialApolloState: apolloClient.cache.extract(),
+//     },
+//   }
+// }
 
 export default Index
