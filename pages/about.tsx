bc1281771f9f0e7d4dc709a5c326c40088612576
Vladka <xvladka@gmail.com>
2021-04-06 13:31:30 +0200

    Graph.js example

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,22 @@
 import Link from 'next/link'
+import { Line } from 'react-chartjs-2'
 
-export default function About() {
+const state = {
+  labels: ['January', 'February', 'March', 'April', 'May'],
+  datasets: [
+    {
+      label: 'Rainfall',
+      fill: false,
+      lineTension: 0.5,
+      backgroundColor: 'rgba(75,192,192,1)',
+      borderColor: 'rgba(0,0,0,1)',
+      borderWidth: 2,
+      data: [65, 59, 80, 81, 56],
+    },
+  ],
+}
+
+const About = () => {
   return (
     <div>
       Welcome to the about page. Go to the{' '}
@@ -8,6 +24,22 @@ export default function About() {
         <a>Home</a>
       </Link>{' '}
       page.
+      <Line
+        data={state}
+        options={{
+          title: {
+            display: true,
+            text: 'Average Rainfall per month',
+            fontSize: 20,
+          },
+          legend: {
+            display: true,
+            position: 'right',
+          },
+        }}
+      />
     </div>
   )
 }
+
+export default About
